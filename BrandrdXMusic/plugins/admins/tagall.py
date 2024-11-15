68271537667c15cb557f3bf5925429340f33a942
SilkSmithavibz <Silksmithavibz@gmail.com>
2024-11-15 16:23:15 +0530

    Update tagall.py

--- BrandrdXMusic/plugins/admins/tagall.py
+++ BrandrdXMusic/plugins/admins/tagall.py
@@ -231,5 +231,5 @@ async def cancel_spam(client, message):
             spam_chats.remove(message.chat.id)
         except:
             pass
-        return await message.reply("🌷 𝐁𝐑𝐀𝐍𝐃𝐄𝐃 𝐓𝐀𝐆 𝐀𝐋𝐋 𝐏𝐑𝐎𝐂𝐄𝐒𝐒 𝐒𝐓𝐎𝐏𝐏𝐄𝐃 🎉")
+        return await message.reply("🌷 𝐓𝐀𝐆 𝐀𝐋𝐋 𝐏𝐑𝐎𝐂𝐄𝐒𝐒 𝐒𝐓𝐎𝐏𝐏𝐄𝐃 🎉")
               
